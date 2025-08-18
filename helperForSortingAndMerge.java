0f3d568bf8dc09cd0dea2a6cb2cc99e4319c48de
devdeves <mdeves143@gmail.com>
2025-08-19 02:06:27 +0530

    Added new helper for sorting and merge

---
+++ helperForSortingAndMerge.java
@@ -0,0 +1,12 @@
+public int[] mergeAndSortHelper(int[] left , int[] right){
+    int[] res = new int[left.length + right.length];
+    int i = 0 ; j = 0 ; k = 0;
+
+    while(i < left.length && j < right.length)
+            res[k++] = (left[i] < right[j]) ? left[i++] : right[j++];
+    while(i < left.length) res[k++] = left[i++];
+    while(j < left.length) res[k++] = left[j++];
+
+    return res; 
+
+}
