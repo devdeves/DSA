9e5a1ca7047cf92e8d160e27a1f80a5f9ca8c1b0
devdeves <mdeves143@gmail.com>
2025-08-25 02:26:44 +0530

    added static class for ll to render and coded whole linked list code to design new linked list

---
+++ StaticLinkedList.java
@@ -0,0 +1,58 @@
+
+// Learn LL step by step 
+class MyLinkedList {
+    // Inner class Node
+    private class Node {
+        int val;       // stores data (the value in this node)
+        Node next;     // pointer reference to the next node 
+
+        Node(int val) { // constructor for Node
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private int size;
+
+    // Default constructor
+    public MyLinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    // Constructor that builds LL from array
+    public MyLinkedList(int[] values) {
+        if (values.length == 0) {
+            head = null;
+            size = 0;
+            return;
+        }
+        head = new Node(values[0]);
+        int counter = 0 ;
+        Node curr = head;
+         while(counter < values.length){
+            curr.next = new Node(values[i]);
+            curr = curr.next;
+            size++;
+            counter++;
+         }
+     
+    }
+
+    // Print the linked list
+    public void printList() {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.val + " -> ");
+            curr = curr.next;
+        }
+        System.out.println("null");
+    }
+
+    // Test class with main method
+    public static void main(String[] args) {
+        MyLinkedList list = new MyLinkedList(new int[]{5, 10, 15, 20});
+        list.printList();  // Output: 5 -> 10 -> 15 -> 20 -> null
+    }
+}
