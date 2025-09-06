84b0a855ed15305ff63a1d21dff963ab88abe622
devdeves <mdeves143@gmail.com>
2025-09-07 02:22:39 +0530

    added code for revision of palindromic ll

---
+++ revision_of_palindromic_ll.java
@@ -0,0 +1,31 @@
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse second half
+        ListNode prev = null;
+        while (slow != null) {
+            ListNode temp = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = temp;
+        }
+
+        // Compare halves
+        ListNode first = head, second = prev;
+        while (second != null) {
+            if (first.val != second.val) return false;
+            first = first.next;
+            second = second.next;
+        }
+
+        return true;
+    }
+}
+    
