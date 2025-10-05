7c17c5ec67209f80f196613e6539488d462d170d
devdeves <mdeves143@gmail.com>
2025-10-06 02:31:20 +0530

    added clumsy_factorial code

---
+++ Stack  & Logical Math/clumsy_factorial.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int clumsy(int n) {
+        int ans = 0; int sign = 1 ;
+
+        for(int i = n ; i > 0 ; i -= 4){
+            int temp = i;
+
+            if(i-1 > 0){temp *= i-1 ;}
+            if(i-2 > 0 ){temp /= i-2 ;}
+
+            ans += temp * sign;
+
+            if(i-3 > 0){ans += i-3 ;}
+            sign = -1;
+        }
+        return ans;
+    }
+}
